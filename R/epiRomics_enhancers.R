bde020bc77a4ffba0c45438ecaa421360d509860
Alex M. Mawla <ammawla@ucdavis.edu>
2021-08-18 12:41:28 -0700

    Update

--- R/epiRomics_enhancers.R
+++ R/epiRomics_enhancers.R
@@ -6,12 +6,11 @@
 #' @return Variable of class epiRomics further exploring candidate enhancer regions identified after histone integration
 #' @export
 epiRomics_enhancers <- function(epiRomics_dB, epiRomics_histone_mark_1 = "h3k4me1", epiRomics_histone_mark_2 = "h3k27ac") {
-    epiRomics_putative <- epiRomics_dB
-    mark1 <- base::paste0(epiRomics_dB@genome, "_custom_", epiRomics_histone_mark_1)
-    mark2 <- base::paste0(epiRomics_dB@genome, "_custom_", epiRomics_histone_mark_2)
-    
-    epiRomics_putative@annotations <- GenomicRanges::intersect(GenomicRanges::reduce(epiRomics_dB@annotations[epiRomics_dB@annotations$type == 
-        mark1, ]), GenomicRanges::reduce(epiRomics_dB@annotations[epiRomics_dB@annotations$type == mark2, 
-        ]))
-    base::return(epiRomics_putative)
+  epiRomics_putative <- epiRomics_dB
+  mark1 <- base::paste0(epiRomics_dB@genome, "_custom_", epiRomics_histone_mark_1)
+  mark2 <- base::paste0(epiRomics_dB@genome, "_custom_", epiRomics_histone_mark_2)
+
+  epiRomics_putative@annotations <- GenomicRanges::intersect(GenomicRanges::reduce(epiRomics_dB@annotations[epiRomics_dB@annotations$type ==
+    mark1, ]), GenomicRanges::reduce(epiRomics_dB@annotations[epiRomics_dB@annotations$type == mark2, ]))
+  base::return(epiRomics_putative)
 }
