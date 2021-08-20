ae26bcd389a394bd3bbb1b6a66ed5b32b32e11d7
Alex M. Mawla <ammawla@ucdavis.edu>
2021-08-20 11:49:19 -0700

    tests

---
+++ tests/testthat/test-test.R
@@ -0,0 +1,14 @@
+
+testthat::test_that("errors", {
+  testthat::expect_s4_class(
+    epiRomics_build_dB(
+      epiRomics_db_file =  system.file(
+        "extdata",
+        "example_epiRomics_Db_sheet_user_paths.csv",
+        package = "epiRomics"
+      ),
+      txdb_organism = "TxDb.Hsapiens.UCSC.hg38.knownGene::TxDb.Hsapiens.UCSC.hg38.knownGene",
+      epiRomics_genome = "hg38",
+      epiRomics_organism = "org.Hs.eg.db"
+    ), "epiRomicsS4")
+})
