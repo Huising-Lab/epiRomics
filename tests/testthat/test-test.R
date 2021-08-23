898358d85ddc7b849559d1387533bd3578372c45
Alex M. Mawla <ammawla@ucdavis.edu>
2021-08-22 17:31:25 -0700

    Update test-test.R

--- tests/testthat/test-test.R
+++ tests/testthat/test-test.R
@@ -1,4 +1,3 @@
-
 testthat::test_that("errors", {
   testthat::expect_s4_class(
     epiRomics_build_dB(
@@ -10,6 +9,7 @@ testthat::test_that("errors", {
       txdb_organism = "TxDb.Hsapiens.UCSC.hg38.knownGene::TxDb.Hsapiens.UCSC.hg38.knownGene",
       epiRomics_genome = "hg38",
       epiRomics_organism = "org.Hs.eg.db"
-    ), "epiRomicsS4"
+    ),
+    "epiRomicsS4"
   )
 })
