86f561da7da0977073c6dc6c90093463e14ee79d
Alex M. Mawla <ammawla@ucdavis.edu>
2021-09-21 11:40:13 -0700

    build

--- inst/dependencies.R
+++ inst/dependencies.R
@@ -1,20 +1,27 @@
 # No Remotes ----
 # Attachments ----
-if (!requireNamespace("BiocManager", quietly = TRUE))
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
   utils::install.packages("BiocManager")
+}
 BiocManager::install()
 
+
 to_install_cran <-
-  c("data.table",
+  c(
+    "data.table",
     "party",
-    "plyr")
+    "plyr",
+    "knitr",
+    "rmarkdown"
+  )
 for (i in to_install_cran) {
   base::packageStartupMessage(paste("looking for ", i))
   if (!requireNamespace(i)) {
     base::packageStartupMessage(paste("     installing", i))
-    utils::install.packages(i)
+    utils::install.packages(i, repos = "http://cran.us.r-project.org")
   }
 }
+
 
 to_install_bc <-
   c(
@@ -34,6 +41,6 @@ for (i in to_install_bc) {
   base::packageStartupMessage(paste("looking for ", i))
   if (!requireNamespace(i)) {
     base::packageStartupMessage(paste("     installing", i))
-    BiocManager::install(i, type = "source")
+    BiocManager::install(i, type = "source", ask=FALSE)
   }
 }
