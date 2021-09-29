720186893c9de02449ceab8b130dacd6c38b8eac
Alex M. Mawla <ammawla@ucdavis.edu>
2021-09-28 18:29:31 -0700

    Vignette update

--- R/zzz.R
+++ R/zzz.R
@@ -31,37 +31,46 @@
         "party",
         "plyr",
         "knitr",
-        "rmarkdown"
-      )
-    for (i in to_install_cran) {
+        "rmarkdown",
+        "igraph"
+      )i in to_install_cran) {
       base::packageStartupMessage(paste("looking for ", i))
       if (!requireNamespace(i)) {
         base::packageStartupMessage(paste("     installing", i))
         utils::install.packages(i, repos = "http://cran.us.r-project.org")
       }
     }
-  
+base::packageStartupMessage(paste("looking for ", i))
+if (!requireNamespace(i)) {
+  base::packageStartupMessage(paste("     installing", i))
+  utils::install.packages(i, repos = "http://cran.us.r-project.org", type="source")
+}
+  }
 
-    to_install_bc <-
-      c(
-        "AnnotationDbi",
-        "annotatr",
-        "BiocGenerics",
-        "GenomicFeatures",
-        "GenomicRanges",
-        "Gviz",
-        "IRanges",
-        "rtracklayer",
-        "org.Hs.eg.db",
-        "TxDb.Hsapiens.UCSC.hg38.knownGene"
-      )
 
-    for (i in to_install_bc) {
-      base::packageStartupMessage(paste("looking for ", i))
-      if (!requireNamespace(i)) {
-        base::packageStartupMessage(paste("     installing", i))
-        BiocManager::install(i, type = "source", ask=FALSE)
-      }
+
+to_install_bc <-
+  c(
+    "AnnotationDbi",
+    "annotatr",
+    "BiocGenerics",
+    "GenomicFeatures",
+    "GenomicRanges",
+    "Gviz",
+    "IRanges",
+    "rtracklayer",
+    "enrichplot",
+    "ChIPseeker",
+    "org.Hs.eg.db",
+    "TxDb.Hsapiens.UCSC.hg38.knownGene"
+  )
+
+for (i in to_install_bc) {
+  base::packageStartupMessage(paste("looking for ", i))
+  if (!requireNamespace(i)) {
+    base::packageStartupMessage(paste("     installing", i))
+    BiocManager::install(i, type = "source", ask=FALSE)
+  }
     }
 
 
